58222ad6351d45503e305e94413c774ec7fcb095
Tammy Callahan <trenec@me.com>
2018-01-22 15:47:20 -0600

    Added CF Portfolio project to work

--- js/work.js
+++ js/work.js
@@ -1,5 +1,11 @@
 var works = [
   {
+    title: "CareerFoundry Portfolio Project",
+    technology: "HTML, CSS, JavaScript, Bootstrap",
+    pic: "img/proj-cfportfolio.png",
+    url: "http://focused-cfportfolio-6874a0.bitballoon.com/"
+  },
+  {
     title: "Calculator",
     technology: "HTML, CSS, JavaScript",
     pic: "img/proj-calculator.png",
