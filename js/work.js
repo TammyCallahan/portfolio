b32fec5a85c581aec2989c2349584178a9cc5ac0
Tammy Callahan <trenec@me.com>
2018-01-23 14:33:34 -0600

    Updated Project screenshots & added CF Portfolio project

--- js/work.js
+++ js/work.js
@@ -1,20 +1,20 @@
 var works = [
   {
-    title: "CareerFoundry Portfolio Project",
+    title: "Portfolio Project",
     technology: "HTML, CSS, JavaScript, Bootstrap",
-    pic: "img/proj-cfportfolio.png",
+    pic: "img/projss-cf-portfolio.png",
     url: "http://focused-cfportfolio-6874a0.bitballoon.com/"
   },
   {
     title: "Calculator",
     technology: "HTML, CSS, JavaScript",
-    pic: "img/proj-calculator.png",
+    pic: "img/projss-calculator.png",
     url: "http://frosty-torvalds-c325f0.bitballoon.com/"
   },
   {
     title: "More to be added soon",
     technology: "n/a",
-    pic: "img/proj-coming-soon.png",
+    pic: "img/projss-coming-soon.png",
     url: ""
   }
 ];
