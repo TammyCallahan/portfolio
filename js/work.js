6150d64ec13369828ac4d13c834b7da6a890cf4c
Tammy Callahan <trenec@me.com>
2018-03-10 15:03:18 -0600

    Added eCommerce project

--- js/work.js
+++ js/work.js
@@ -2,17 +2,17 @@ var works = [
   {
     title: "Portfolio Project",
     technology: "HTML, CSS, JavaScript, Bootstrap",
-    description: "Course project to develop a portfolio website template with items such as a carousel, parallax effect, embedded video, contact form, and Google map.",
+    description: "Portfolio website template with items such as a carousel, parallax effect, embedded video, contact form, and Google map.",
     pic: "img/projss-cf-portfolio.png",
     github: "http://gist.github.com/TammyCallahan/b32f8ea11606183ba448e7528ceb2fd0",
     url: "http://focused-cfportfolio-6874a0.bitballoon.com/"
   },
   {
-    title: "Simple Calculator",
-    technology: "HTML, CSS, JavaScript",
-    description: "Course project to develop a calculator with HTML, CSS, and JavaScript only.",
-    pic: "img/projss-calculator.png",
-    github: "http://github.com/TammyCallahan/simple-calculator",
-    url: "http://frosty-torvalds-c325f0.bitballoon.com/"
+    title: "eCommerce Project",
+    technology: "HTML, CSS, JavaScript, Ruby on Rails",
+    description: "eCommerce site utilizing Ruby on Rails with Stripe payment, authorization and authentication using the Devise and CanCanCan gems, and tested utilizing RSpec and Factory Bot.",
+    pic: "img/projss-ecommerce.png",
+    github: "http://github.com/TammyCallahan/FirstRailsApp",
+    url: "http://bike-berlin-ruby-app.herokuapp.com/"
   }
 ];
